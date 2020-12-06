15d03e6ebb28561dc0f1c2ff99cf862cd2de34bd
NoamNol <noamnol19@gmail.com>
2020-12-06 16:12:10 +0200

    Add PyPI package setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gematriapy",
+    version="0.0.1",
+    author="Noam Nol",
+    author_email="noamnol19@gmail.com",
+    description="Convert numbers to Hebrew letters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NoamNol/gematriapy",
+    packages=['gematriapy'],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 4 - Beta',
+    ],
+    keywords="hebrew gematria gematriapy",
+    python_requires='>=3.7',
+)
