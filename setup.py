56fe47635ce627697cb4dab30b1dbaa44bcf4c31
NoamNol <noamnol19@gmail.com>
2020-12-06 16:28:24 +0200

    Change version to v0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gematriapy",
-    version="0.0.1",
+    version="0.1.0",
     author="Noam Nol",
     author_email="noamnol19@gmail.com",
     description="Convert numbers to Hebrew letters",
